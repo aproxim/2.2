8ded1491afc9f4c2d7440c052ea1c8057b5bd708
Olga Kosheleva <aproxim@rambler.ru>
2018-03-14 01:49:38 +0400

    first commit

---
+++ test.php
@@ -0,0 +1,82 @@
+<?php
+// Если не был передан номер теста - возвращать на страницу со списком тестов
+if (isset($_GET['number']) === false) {
+    header('Location: list.php');
+    exit;
+}
+// Получаем файл с номером из GET-запроса
+$allTests = glob('tests/*.json');
+$number = $_GET['number'];
+$test = file_get_contents($allTests[$number]);
+$test = json_decode($test, true);
+// Если была нажата кнопка проверки теста, то проверить и вывести результат
+if (isset($_POST['check-test'])) {
+    function checkTest($testFile) {
+        foreach ($testFile as $key => $item) {
+            if (!isset($_POST['answer' . $key])) {
+                echo 'Должны быть решены все задания!';
+                exit;
+            }
+        }
+        $i = 0;
+        $questions = 0;
+        foreach ($testFile as $key => $item) {
+            $questions++;
+             // А также прибавляется 1 к переменной $i, если ответ правильный
+            if ($item['correct_answer'] === $_POST['answer' . $key]) {
+                $i++;
+              
+            }
+            // Вывод блока с вопросом и ответом
+            echo 'Вопрос: ' . $item['question'] . '<br>';
+            echo 'Ваш ответ: ' . $item['answers'][$_POST['answer' . $key]] . '<br>';
+            echo 'Правильный ответ: ' . $item['answers'][$item['correct_answer']] . '<br>';
+            echo '</div>';
+            echo '<hr>';
+        }
+        echo '<p style="font-weight: bold;">Итого правильных ответов: ' . $i . ' из ' . $questions . '</p>';
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <title>Document</title>
+   
+</head>
+<body>
+
+    <!-- Если пользователь находиться на тесте, ссылка введет на страницу с тестами, если пользователь отправил тест на проверку и видит результаты, то возвращает на страницу с тестом -->
+
+    <a href="<?php echo isset($_POST['check-test']) ? $_SERVER['HTTP_REFERER'] : 'list.php' ?>"><div>< Назад</div></a><br>
+
+    <!-- Если передан номер теста в GET-запросе и пользователь еще не нажал на кнопку проверки теста, то выводить тест -->
+
+    <?php if (isset($_GET['number']) && !isset($_POST['check-test'])): ?>
+        <form method="POST">
+            <h1><?php echo basename($allTests[$number]); ?></h1>
+            <?php foreach($test as $key => $item):  ?>
+            <fieldset>
+                <legend><?php echo $item['question'] ?></legend>
+                <div class="on-hidden-radio"></div>
+                <input type="radio" name="answer<?php echo $key ?>" id="hidden-radio" required>
+                <label><input type="radio" name="answer<?php echo $key ?>" value="0"><?php echo $item['answers'][0] ?></label><br>
+                <label><input type="radio" name="answer<?php echo $key ?>" value="1"><?php echo $item['answers'][1] ?></label><br>
+                <label><input type="radio" name="answer<?php echo $key ?>" value="2"><?php echo $item['answers'][2] ?></label><br>
+                <label><input type="radio" name="answer<?php echo $key ?>" value="3"><?php echo $item['answers'][3] ?></label>
+            </fieldset>
+            <?php endforeach; ?>
+            <input type="submit" name="check-test" value="Проверить">
+        </form>
+    <?php endif; ?>
+
+    <!-- Если пользователь нажал на кнопку проверки теста, то выводить ему результаты -->
+
+    <div class="check-test">
+        <?php if (isset($_POST['check-test'])) echo checkTest($test); ?>
+    </div>
+
+</body>
+</html>
